b2b73c764d8147dfb39f0279b0cdb36a03967f13
Adbelrhman Gomaa <abdelrhman.gomaa@baianat.com>
2023-04-27 12:37:59 +0200

    feat: user schema to mongodb

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,18 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { User, UserSchema } from './models/user.model';
 
 @Module({
-    imports:[],
+    imports: [
+        MongooseModule.forFeature(
+            [
+                { name: User.name, schema: UserSchema },
+            ]
+        ),
+    ],
     controllers: [],
     providers: [UserService],
     exports: [UserService]
 })
-export class UserModule{}
+export class UserModule { }
