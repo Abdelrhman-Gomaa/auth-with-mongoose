fbe1885c28595d5b58ca99d0e6cb2f65c885c136
Adbelrhman Gomaa <abdelrhman.gomaa@baianat.com>
2023-04-27 12:05:19 +0200

    feat: user schema

---
+++ src/user/models/user.model.ts
@@ -0,0 +1,58 @@
+import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from 'mongoose';
+import { UserRoleEnum } from '../user.enum';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+    @Prop({ required: true })
+    firstName: string;
+
+    @Prop({ required: true })
+    lastName: string;
+
+    @Prop({ required: true })
+    fullName: string;
+
+    @Prop({
+        type: String,
+        required: true,
+        unique: true,
+    })
+    userName: string;
+
+    @Prop({
+        type: String,
+    })
+    notVerifiedEmail?: string;
+
+    @Prop({
+        type: String,
+        unique: true,
+    })
+    verifiedEmail?: string;
+
+    @Prop()
+    slug: string;
+
+    @Prop({ required: true })
+    password: string;
+
+    @Prop()
+    phoneNumber: string;
+
+    @Prop()
+    nation: string;
+
+    @Prop()
+    birthDay: Date;
+
+    @Prop({
+        type: String,
+        enum: UserRoleEnum
+    })
+    role: UserRoleEnum;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
