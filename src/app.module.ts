9f1089d97b1fde58aed76a5dfcd180b4e4100408
Adbelrhman Gomaa <abdelrhman.gomaa@baianat.com>
2023-04-27 11:22:02 +0200

    feat: mongoose odm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+      {
+        connectionFactory: (connection) => {
+          connection.plugin(require('mongoose-autopopulate'));
+          connection.plugin(require('mongoose-paginate-v2'));
+          return connection;
+        }
+      }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
