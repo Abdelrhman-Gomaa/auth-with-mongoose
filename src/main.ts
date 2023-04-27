095161eceadf296fb9a4d69a7105824359280c7e
Adbelrhman Gomaa <abdelrhman.gomaa@baianat.com>
2023-04-27 12:46:06 +0200

    feat: swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth With Mongoose')
+    .setDescription('The Authentication API With Mongoose')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('auth-with-mongoose', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
